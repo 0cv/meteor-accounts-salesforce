935ba9fcc1813092c53d406194adf1567f44cf2c
Christophe Vidal <krisallyde@gmail.com>
2015-12-15 21:58:31 +0100

    Update package.js

--- package.js
+++ package.js
@@ -1,15 +1,21 @@
 Package.describe({
-  summary: "Accounts service for Salesforce accounts"
+  name: 'salesforce:accounts-salesforce',
+  version: '0.1.0',
+  summary: "Accounts service for Salesforce accounts",
+  git: 'https://github.com/Krisa/meteor-accounts-salesforce.git',
+  documentation: 'README.md'
 });
 
-Package.on_use(function(api) {
+Package.onUse(function(api) {
+  api.versionsFrom('1.2.0');
+  
   api.use('accounts-base', ['client', 'server']);
   api.use('accounts-oauth', ['client', 'server']);
-  api.use('salesforce', ['client', 'server']);
+  api.use('salesforce:salesforce@0.0.1', ['client', 'server']);
 
-  api.add_files(['salesforce_login_button.css'], 'client');
+  api.addFiles(['salesforce_login_button.css'], 'client');
 
-  api.add_files('salesforce_common.js', ['client', 'server']);
-  api.add_files('salesforce_server.js', 'server');
-  api.add_files('salesforce_client.js', 'client');
+  api.addFiles('salesforce_common.js', ['client', 'server']);
+  api.addFiles('salesforce_server.js', 'server');
+  api.addFiles('salesforce_client.js', 'client');
 });
