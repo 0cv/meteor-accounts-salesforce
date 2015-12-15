89f4dbac09e0b9ff92d0f7e4afd0d1eb55f37953
Christophe Vidal <krisallyde@gmail.com>
2015-12-15 22:43:55 +0100

    Update salesforce_server.js

--- salesforce_server.js
+++ salesforce_server.js
@@ -1,9 +1,11 @@
-Accounts.addAutopublishFields({
-  // publish all fields including access token, which can legitimately
-  // be used from the client (if transmitted over ssl or on
-  // localhost).
-  forLoggedInUser: ['services.salesforce'],
-  forOtherUsers: [
-    'services.salesforce.username'
-  ]
-});
+if (Meteor.isServer) {
+  Accounts.addAutopublishFields({
+    // publish all fields including access token, which can legitimately
+    // be used from the client (if transmitted over ssl or on
+    // localhost).
+    forLoggedInUser: ['services.salesforce'],
+    forOtherUsers: [
+      'services.salesforce.username'
+    ]
+  });
+}
